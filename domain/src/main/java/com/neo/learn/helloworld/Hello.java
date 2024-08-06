3ca669bd5ea9bcd212cfbe48b5fea603dc33b67f
giovannybrandalise <giovannybrandalise@brq.com>
2024-08-06 20:15:23 +0000

    criando novo modulo domain e deletando modulo compiler

---
+++ domain/src/main/java/com/neo/learn/helloworld/Hello.java
@@ -0,0 +1,9 @@
+package com.neo.learn.helloworld;
+
+public class Hello {
+
+    public String grettings(String name) {
+        return "Hello " + name;
+    }
+
+}
---
+++ domain/src/test/java/com/neo/learn/helloworld/HelloTest.java
@@ -0,0 +1,30 @@
+package com.neo.learn.helloworld;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@Test
+	void test_instance() {
+		var entity = new Hello();
+		assertNotNull(entity);
+	}
+
+	@Test
+	void test_grettings_with_name() {
+		var entity = new Hello();
+		var result = entity.grettings("Test");
+		assertEquals("Hello Test", result);
+	}
+
+	@Test
+	void test_grettings_with_null() {
+		var entity = new Hello();
+		var result = entity.grettings(null);
+		assertEquals("Hello null", result);
+	}
+
+}

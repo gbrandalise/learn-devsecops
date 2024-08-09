a39b5ab2b37cab24b684375b273d1368a120f869
giovannybrandalise <giovannybrandalise@brq.com>
2024-08-09 19:46:10 +0000

    ajustando portas

--- app/k8s/pod/apply.sh
+++ app/k8s/pod/apply.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f ./app/k8s/pod/pod.yml && \
+sleep 30 && \
+kubectl describe pod devsecops && \
 kubectl get pods && \
-kubectl describe pod devsecops
+kubectl port-forward pod/devsecops 8080:8080

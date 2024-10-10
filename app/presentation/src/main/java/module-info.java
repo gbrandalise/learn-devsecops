facd4be8199028b1b7738102f11f7a355b7dfec8
giovannybrandalise <giovannybrandalise@brq.com>
2024-10-10 16:34:15 -0300

    configurando o actuactor para prometheus

--- app/presentation/src/main/java/module-info.java
+++ app/presentation/src/main/java/module-info.java
@@ -1,13 +1,19 @@
 module com.neo.learn.presentation {
-    requires com.neo.learn.application;
 
 	requires spring.boot;
 	requires spring.boot.autoconfigure;
     requires spring.context;
+	requires spring.core;
     requires spring.beans;
 	requires spring.web;
 
+	requires java.instrument;
+
+    requires com.neo.learn.application;
+
+	exports com.neo.learn.presentation;
 	exports com.neo.learn.presentation.hello;
 
 	opens com.neo.learn.presentation to spring.core;
+
 }

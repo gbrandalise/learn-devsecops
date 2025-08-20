7eb67487ee315ba96f5b543e1eceed17b07437d8
giovannybrandalise <giovannybrandalise@brq.com>
2025-08-20 13:32:11 +0000

    configurando replicaset

--- app/k8s/pod/apply.sh
+++ app/k8s/pod/apply.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 kubectl apply -f ./app/k8s/pod/pod.yml && \
-sleep 60 && \
-kubectl describe pod devsecops && \
-kubectl get pods && \
-kubectl port-forward --address 0.0.0.0 pod/devsecops 8080:8080
+watch 'kubectl get pods' && \
+kubectl port-forward --address 0.0.0.0 pod/devsecops-latest 8080:8080

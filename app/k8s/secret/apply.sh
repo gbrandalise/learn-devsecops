242056bcf709be269ec15a93be56ba7c9d7b742f
giovannybrandalise <giovannybrandalise@brq.com>
2025-08-28 12:52:46 +0000

    Update greeting message format and switch from ConfigMap to Secret for environment variables

--- app/k8s/secret/apply.sh
+++ app/k8s/secret/apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#kubectl create secret generic devsecops-secret --from-literal=GREETING_MESSAGE="Hello %s\n"
-export GREETINGS_MESSAGE=$(echo -n "Hello %s\n" | base64) && \
+#kubectl create secret generic devsecops-secret --from-literal=GREETING_MESSAGE="Hello %s"
+export GREETINGS_MESSAGE=$(echo -n "Hello %s" | base64) && \
 envsubst < ./app/k8s/secret/secret.yml | kubectl apply -f - && \
 kubectl describe secret devsecops-secret

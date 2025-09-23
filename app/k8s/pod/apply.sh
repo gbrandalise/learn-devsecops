62a7260a1e6d02f8129f0f09f432c81222b2fc77
giovannybrandalise <giovannybrandalise@brq.com>
2025-09-23 19:02:09 +0000

    Add restartPolicy and fix pod configuration in Kubernetes manifests

--- app/k8s/pod/apply.sh
+++ app/k8s/pod/apply.sh
@@ -2,4 +2,4 @@
 
 kubectl apply -f ./app/k8s/pod/pod.yml && \
 watch 'kubectl get pods' && \
-kubectl port-forward --address 0.0.0.0 pod/devsecops-latest 8080:8080
+kubectl port-forward --address 0.0.0.0 pod/devsecops-pt 8080:8080

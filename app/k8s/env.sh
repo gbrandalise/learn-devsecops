41a9bf012f6df201d98aeb2f00452ebb6317ffa9
giovannybrandalise <giovannybrandalise@brq.com>
2025-12-01 20:34:33 +0000

    Replace base64-encoded environment variables with plain text values in Kubernetes configurations

--- app/k8s/env.sh
+++ app/k8s/env.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 export NAMESPACE="development" && \
-export POSTGRES_HOST=$(echo -n "postgresql" | base64) && \
-export POSTGRES_PORT=$(echo -n "5432" | base64) && \
+export POSTGRES_HOST_PLAIN="postgresql" && \
+export POSTGRES_PORT_PLAIN="5432" && \
 export POSTGRES_DB_PLAIN="devsecops" && \
 export POSTGRES_USER_PLAIN="devsecops" && \
 export POSTGRES_PASSWORD_PLAIN="devsecops" && \
+export GREETINGS_MESSAGE_PLAIN="Hello %s" && \
+export POSTGRES_HOST=$(echo -n ${POSTGRES_HOST_PLAIN} | base64) && \
+export POSTGRES_PORT=$(echo -n ${POSTGRES_PORT_PLAIN} | base64) && \
 export POSTGRES_DB=$(echo -n ${POSTGRES_DB_PLAIN} | base64) && \
 export POSTGRES_USER=$(echo -n ${POSTGRES_USER_PLAIN} | base64) && \
 export POSTGRES_PASSWORD=$(echo -n ${POSTGRES_PASSWORD_PLAIN} | base64) && \
-export GREETINGS_MESSAGE=$(echo -n "Hello %s" | base64)
+export GREETINGS_MESSAGE=$(echo -n ${GREETINGS_MESSAGE_PLAIN} | base64)

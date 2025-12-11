b98926a76b629b418725ba39cbe1adc7bc338f47
giovannybrandalise <giovanny.brandalise@gmail.com>
2025-12-11 19:35:46 +0000

    Configure git user identity and comment out safe directory setting

--- tools/git/config.sh
+++ tools/git/config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git config --global --add safe.directory $1
+git config --global --replace-all user.name "giovannybrandalise"
+git config --global --replace-all user.email "giovanny.brandalise@gmail.com"
+#git config --global --add safe.directory $1

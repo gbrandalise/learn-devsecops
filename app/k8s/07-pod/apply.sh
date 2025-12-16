e5dc1bc828a7d4d4f9d7d3dc4fb483c8f5ff8ceb
giovannybrandalise <giovanny.brandalise@gmail.com>
2025-12-16 19:46:25 +0000

    Add persistent storage to PostgreSQL deployment and reorganize k8s resources
    - Add PGDATA environment variable and volume mounts to PostgreSQL deployment
    - Configure hostPath volume for PostgreSQL data persistence
    - Reorganize k8s resources: move pods to 07-pod, replicasets to 09-replicaset, services to 10-service
    - Add multiple service types: ClusterIP, ExternalName, LoadBalancer, and NodePort
    - Update resource quota limits for development-db namespace
    - Include apply scripts for all new resource configurations

--- app/k8s/07-pod/apply.sh
+++ app/k8s/07-pod/apply.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ./app/k8s/env.sh && \
-envsubst < ./app/k8s/06-pod/pods.yml | kubectl apply -f - && \
+envsubst < ./app/k8s/07-pod/pods.yml | kubectl apply -f - && \
 watch 'kubectl get pod -A | grep -E "'${NAMESPACE}'-db|'${NAMESPACE}'-app"'
--- app/k8s/10-service/04-nodeport/apply.sh
+++ app/k8s/10-service/04-nodeport/apply.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ./app/k8s/env.sh && \
-envsubst < ./app/k8s/09-service/01-clusterip/services.yml | kubectl apply -f - && \
+envsubst < ./app/k8s/10-service/04-nodeport/services.yml | kubectl apply -f - && \
 watch 'kubectl get service,po -A | grep -E "'${NAMESPACE}'-db|'${NAMESPACE}'-app"'

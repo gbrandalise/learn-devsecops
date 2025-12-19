463b66b756d5b73df3377ac581071da63d47796a
giovannybrandalise <giovanny.brandalise@gmail.com>
2025-12-19 14:25:43 +0000

    Remove load-env.sh from devcontainer post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 ./.devcontainer/install-dependencies.sh && \
-./.devcontainer/load-env.sh && \
 ./tools/git/config.sh && \
 ./tools/github/create-runner.sh && \
 ./tools/k8s/kind/create-cluster.sh && \

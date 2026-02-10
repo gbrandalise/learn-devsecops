6c94ed5bcc4153e0338e6087a3dfea2deb0491ea
giovannybrandalise <giovanny.brandalise@gmail.com>
2026-02-10 21:13:14 +0000

    Add shell script copying to GitHub Actions runner setup

--- tools/github/start-runner.sh
+++ tools/github/start-runner.sh
@@ -4,6 +4,10 @@ WORKDIR="$(pwd)"
 RUNNER_DIR="${WORKDIR}/tools/github/actions-runner"
 
 if [[ -d "${RUNNER_DIR}" ]]; then
+	echo "Enter the runner directory"
+	cd "${RUNNER_DIR}" && \
+	echo "Copying .sh files to the runner directory"
+	cp ../*.sh* ./ && \
 	echo "Running the runner!"
 	nohup ./run.sh > nohup.out 2>&1 &
 	echo "Return to the original directory"

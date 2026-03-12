d19f3006cab51aedbaca665bd6aae3ffe5b16721
giovannybrandalise <giovanny.brandalise@gmail.com>
2026-03-12 17:39:33 +0000

    Add init container for PostgreSQL readiness check and container lifecycle hooks

--- app/k8s/apply.sh
+++ app/k8s/apply.sh
@@ -4,4 +4,5 @@ source ./app/k8s/env.sh && \
 for f in $(find ./app/k8s/ -name "*.yml"); do
 	envsubst < $f | kubectl apply -f -
 done
+./tools/k8s/kubectx/current-namespace-development-app.sh && \
 kubectl get svc,po -A | grep -E "${NAMESPACE}" || true
